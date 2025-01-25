310dbdadbfb6887e1095e8203be16cb6a98d2bc9
fmacedosantos <felipemacedo986@gmail.com>
2025-01-24 22:42:01 -0300

    Criando os migrations

---
+++ migrations/20250124210604-create-positions.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    await queryInterface.createTable('Positions', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      salary: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    
+    await queryInterface.dropTable('Positions')
+  }
+};
