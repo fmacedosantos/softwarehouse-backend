96faa2b21a4f16b92dbdd8b22242a98b788ab1f7
fmacedosantos <felipemacedo986@gmail.com>
2025-01-24 23:01:39 -0300

    Configurando o cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const positionsRouter = require('./routes/positions.routes')
 const employeesRouter = require('./routes/employees.routes')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors({
+  origin: process.env.CORS_ORIGIN
+}))
 
 app.use('/positions', positionsRouter)
 app.use('/employees', employeesRouter)
