d054292eecf0bb498d2bdd2acf5dcc46328c63ef
fmacedosantos <felipemacedo986@gmail.com>
2025-01-25 00:40:14 -0300

    Criando as rotas de user

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/login', userController.login);
+
+router.post('/create', userController.createUser);
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
+const usersRouter = require('./routes/users.routes')
 const positionsRouter = require('./routes/positions.routes')
 const employeesRouter = require('./routes/employees.routes')
 
 const app = express()
 
-app.use(express.json())
+app.use(bodyParser.json())
 app.use(cors({
   origin: process.env.CORS_ORIGIN
 }))
 
+app.use('/users', usersRouter)
 app.use('/positions', positionsRouter)
 app.use('/employees', employeesRouter)
 
