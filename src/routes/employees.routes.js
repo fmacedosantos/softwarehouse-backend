01889435a17c756bf52f20cf6744f55b302f5e71
fmacedosantos <felipemacedo986@gmail.com>
2025-01-25 16:39:52 -0300

    Adicionando as verificações nas rotas

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -2,17 +2,18 @@ const express = require('express')
 const employeeController = require('../controllers/employeeController')
 const checkToken = require('../middlewares/checkToken')
 const validateEmployees = require('../middlewares/validateEmployees')
+const checkRoles = require('../middlewares/checkRoles')
 
 const router = express.Router()
 
-router.get('/', checkToken, employeeController.findAllEmployees)
+router.get('/', checkToken, checkRoles(['employee', 'manager', 'admin']), employeeController.findAllEmployees)
 
-router.get('/:id', validateEmployees.validateFindEmployee, checkToken, employeeController.findOneEmployee)
+router.get('/:id', validateEmployees.validateFindEmployee, checkToken, checkRoles(['employee', 'manager', 'admin']), employeeController.findOneEmployee)
 
-router.post('/', validateEmployees.validateCreateEmployee, checkToken, employeeController.createEmployee)
+router.post('/', validateEmployees.validateCreateEmployee, checkToken, checkRoles(['manager', 'admin']), employeeController.createEmployee)
 
-router.put('/:id', validateEmployees.validateUpdateEmployee, checkToken, employeeController.updateEmployee)
+router.put('/:id', validateEmployees.validateUpdateEmployee, checkToken, checkRoles(['manager', 'admin']), employeeController.updateEmployee)
 
-router.delete('/:id', validateEmployees.validateDeleteEmployee, checkToken, employeeController.deleteEmployee)
+router.delete('/:id', validateEmployees.validateDeleteEmployee, checkToken, checkRoles(['admin']), employeeController.deleteEmployee)
 
 module.exports = router
